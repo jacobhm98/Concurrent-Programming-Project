ebfebed0ee0656c09285caa12f87c159470a63d6
jacobhm98 <jacmalm@kth.se>
2020-03-12 15:25:30 +0100

    made methods public that should be public

--- Multigrid/Jacobi.h
+++ Multigrid/Jacobi.h
@@ -7,10 +7,8 @@ using std::vector;
 class Jacobi {
 	void updateMatrix(vector<vector<vector<double>>> &Matrix);
 	double maxDiff(vector<vector<vector<double>>> &Matrix);
-	void printMatrix(vector<vector<vector<double>>> &Matrix, int);
 	public:
-	//constructors
-	Jacobi(int numIters, vector<vector<vector<double>>> &Matrix);
-	Jacobi(vector<vector<vector<double>>> &Matrix);
-	//method declarations
+	void iterate(int numIters, vector<vector<vector<double>>> &Matrix);
+	void iterate(vector<vector<vector<double>>> &Matrix);
+	void printMatrix(vector<vector<vector<double>>> &Matrix, int);
 };
