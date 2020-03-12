f49d548c4a9e166a2c1bd8052126568876e24683
jacobhm98 <jacmalm@kth.se>
2020-03-12 16:53:48 +0100

    refactored concurrent version of multigrid

---
+++ Multigrid/multigridConc/JacobiConcurrent.cpp
@@ -0,0 +1,80 @@
+
+#include <omp.h>
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include "../Jacobi.h"
+using std::cout;
+using std::endl;
+using std::vector;
+
+//iterate number passed in for the coarse grid
+void Jacobi::iterateP(int numWorkers, int numIters, vector<vector<vector<double>>> &Matrix){
+	for (int i = 0; i < numIters; ++i){
+		updateMatrix(numWorkers, Matrix);
+	}
+}
+
+//iterate four times on the finer grid
+void Jacobi::iterateP(int numWorkers, vector<vector<vector<double>>> &Matrix){
+	for (int i = 0; i < 4; ++i){
+		updateMatrix(numWorkers, Matrix);
+	}
+}
+
+//Laplace's PDE, or Nabla² takes the limit of each point tending towards the average of its neighbours points, 
+//and how this varies with time. This is why it is sometimes called the heat equation: given an input in 1d, 2d, 3d ... 
+//where each point has a value corresponding to its temperature, where the temperature is given as f(x,y,z..) , as time -> infinity,
+//the temperatures will reach an equilibrium across the entire input space. A hot point surrounded by very cold points will get colder quickly
+//and the cold points will get slightly warmer, until all of the points reach the same value. Laplaces PDE: Nabla²(f(x,y,z...) = 0.
+//This method simulates this, by updating each point on the matrix to the average of its neighbouring 4 points.
+void Jacobi::updateMatrix(int numWorkers, vector<vector<vector<double>>> &Matrix){
+	int gridSize = Matrix[0].size();
+	
+	//set the number of threads we wish to use, taken from the commandline. Do the parallelize the for loop with omp.	
+	omp_set_num_threads(numWorkers);
+	#pragma omp parallel for
+	//do the calculations for all internal points of the grid (not boundary points)
+	for (int i = 1; i < gridSize - 1; ++i){
+		for (int j = 1; j < gridSize - 1; ++j){
+			Matrix[1][i][j] = (Matrix[0][i-1][j]+Matrix[0][i+1][j]+Matrix[0][i][j-1]+Matrix[0][i][j+1]) * .25;
+		}
+	}
+	
+	//make new matrix old matrix to prepare for next iteration, save old matrix in new matrix so we can calculate maxdiff still
+	vector<vector<double>> temp;
+	vector<vector<double>> * p1 = &Matrix[0];
+	vector<vector<double>> * p2 = &Matrix[1];
+	temp = *p2;
+	*p2 = *p1;
+	*p1 = temp;
+}
+//Calculate the values delta of each point from this iteration and last, and return the value of the highest one. The smaller this value is,
+//the less effective each subsequent iteration is, and the closer we are to the limit.
+double Jacobi::maxDiff(vector<vector<vector<double>>> &Matrix){
+	int gridSize = Matrix[0].size();
+	double maxDiff = 0;
+	for (int i = 0; i < gridSize; ++i){
+		for (int j = 0; j < gridSize; ++j){
+			double currentDiff = (1 - Matrix[0][i][j]);
+			if (maxDiff < currentDiff){
+				maxDiff = currentDiff;
+			}
+			}
+		}
+	return maxDiff;
+}
+
+//For debugging.
+void Jacobi::printMatrix(vector<vector<vector<double>>> &Matrix, int current){
+	cout << "hello" << endl;
+	int gridSize = Matrix[0].size();
+	for (int i = 0; i < gridSize; ++i){
+		for (int j = 0; j < gridSize; ++j){
+			cout << "|";
+			cout << Matrix[current][i][j];
+			cout << "|";
+		}
+		cout << endl;
+	} 
+}
---
+++ Multigrid/multigridConc/multigridConc.cpp
@@ -0,0 +1,183 @@
+#include "../Jacobi.h"
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <chrono>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+//function declarations
+void restrict(vector<vector<vector<double>>> &Matrix);
+void interpolate(vector<vector<vector<double>>> &Matrix);
+void initializeGrid(int gridSize, vector<vector<vector<double>>> &Matrix);
+void resize(int gridSize, vector<vector<vector<double>>> &Matrix);
+void setDirichletBoundaryConditions(vector<vector<vector<double>>> &Matrix);
+void printMatrix(vector<vector<vector<double>>> &Matrix, int current);
+
+int main (int argc, char * argv[]){
+	int numIters = 0;
+	int gridSize = 0;
+	int numWorkers = 0;
+	if (argc != 4){
+		cout << "Arguments should be gridSize, numIters, and numWorkers" << endl;
+		return 1;
+	}
+	else{
+		gridSize =  atoi(argv[1]);
+		numIters = atoi(argv[2]);
+		numWorkers = atoi(argv[3]);
+	}
+	if ((gridSize - 5) % 4 != 0){
+		cout << "((gridsize - 5) % 4) == 0) for the reduction to work properly!" << endl;
+		return 1;
+	}
+	
+	//Matrix we want to do computations on
+	vector<vector<vector<double>>> Matrix;
+	Jacobi jacobi;
+	initializeGrid(gridSize, Matrix);
+
+	for (int i = 0; i < 3; ++i){
+		jacobi.iterateP(numWorkers, Matrix);
+		restrict(Matrix);
+		printMatrix(Matrix, 0);
+	}
+
+	//iterate on the coarsest level
+	restrict(Matrix);
+	jacobi.iterateP(numWorkers, numIters, Matrix);
+	printMatrix(Matrix, 0);
+
+	for (int i = 0; i < 3; ++i){
+		interpolate(Matrix);
+		jacobi.iterateP(numWorkers, Matrix);
+		printMatrix(Matrix, 0);
+	}
+	
+
+	//begin the computations, start the timer right before
+	auto startTime = std::chrono::high_resolution_clock::now();
+	auto endTime = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+	//print out the specified data
+	//printf("Grid Size, and Number of Iterations: %d, %d\n", gridSize, numIters);
+	//cout << "Execution time of the computational part, in microseconds: " << duration.count() << endl;
+	//cout << "The largest change an arbitrary grid went through this cycle is: " << maxDifference << endl;
+	//print out the state of the matrix to filedata.out
+	std::ofstream out;
+	out.open("./filedata.out");
+	for (int i = 0; i < gridSize; ++i){
+		for (int j = 0; j < gridSize; ++j){
+			out << "|" << Matrix[0][i][j] << "|";
+		}
+		out << endl;
+	}	
+
+	return 0;
+}
+
+
+void restrict(vector<vector<vector<double>>> &Matrix){
+	int newSize = (Matrix[0].size() + 1) / 2;
+	vector<vector<vector<double>>> tempMatrix;	
+	//initialize the temp matrix to what we want
+	initializeGrid(newSize, tempMatrix);
+	
+	//for each interior point, update it according to G. Andrews textbook p. 550-51
+	for (int i = 1; i < newSize - 1; ++i){
+		for (int j = 1; j < newSize - 1; ++j){
+			int correspondingI = 2*i;
+			int correspondingJ = 2*j;
+			tempMatrix[0][i][j] = Matrix[0][correspondingI][correspondingJ] * 0.5 + (Matrix[0][correspondingI - 1][correspondingJ] + Matrix[0][correspondingI + 1][correspondingJ] + Matrix[0][correspondingI][correspondingJ - 1] + Matrix[0][correspondingI][correspondingJ + 1]) * 0.125;
+
+		}
+	}
+
+	Matrix = tempMatrix;
+}
+
+void interpolate(vector<vector<vector<double>>> &Matrix){
+	int newSize = Matrix[0].size() * 2 - 1;
+	vector<vector<vector<double>>> tempMatrix;
+
+	//initialize the new matrix to size we want + boundary conditions
+	initializeGrid(newSize, tempMatrix);
+
+	//iterate only over directly corresponding points	
+	for (int i = 2; i < newSize - 1; i += 2){
+		for (int j = 2; j < newSize - 1; j += 2){
+			//update the directly corresponding points
+			int correspondingI = i/2;
+			int correspondingJ = j/2;
+			tempMatrix[0][i][j] = Matrix[0][correspondingI][correspondingJ];
+			
+			//update the points directly next to directly corresponding points
+			tempMatrix[0][i - 1][j] += tempMatrix[0][i][j] * 0.5;
+			tempMatrix[0][i + 1][j] += tempMatrix[0][i][j] * 0.5;
+			tempMatrix[0][i][j - 1] += tempMatrix[0][i][j] * 0.5;
+			tempMatrix[0][i][j + 1] += tempMatrix[0][i][j] * 0.5;
+			
+			//update the adjacent points
+			tempMatrix[0][i - 1][j - 1] += tempMatrix[0][i][j] * 0.25;
+			tempMatrix[0][i - 1][j + 1] += tempMatrix[0][i][j] * 0.25;
+			tempMatrix[0][i + 1][j - 1] += tempMatrix[0][i][j] * 0.25;
+			tempMatrix[0][i + 1][j + 1] += tempMatrix[0][i][j] * 0.25;
+		}
+	}
+	Matrix = tempMatrix;
+}
+
+//a method which takes the gridsize and initializes our matrix to what it's supposed to be (border cells = 1, everything else = 0)
+void initializeGrid(int gridSize, vector<vector<vector<double>>> &Matrix){
+	//initialize the global matrix
+	resize(gridSize, Matrix);
+
+	//set boundary values to 1
+	setDirichletBoundaryConditions(Matrix);
+
+	//set internal values to 0
+	for (int i = 1; i < gridSize - 1; ++i){
+		for (int j = 1; j < gridSize - 1; ++j){
+			Matrix[0][i][j] = 0;
+		}
+	}
+}
+
+void resize(int gridSize, vector<vector<vector<double>>> &Matrix){
+	Matrix.resize(2);
+	Matrix[0].resize(gridSize);
+	Matrix[1].resize(gridSize);
+	for (int i = 0; i < gridSize; ++i){
+		Matrix[0][i].resize(gridSize);
+		Matrix[1][i].resize(gridSize);
+	}
+	
+}
+void setDirichletBoundaryConditions(vector<vector<vector<double>>> &Matrix){
+
+	int gridSize = Matrix[0].size();
+	for (int i = 0; i < gridSize; i += gridSize - 1){
+		for (int j = 0; j < gridSize; ++j){
+			Matrix[0][i][j] = 1;
+			Matrix[0][j][i] = 1;
+			Matrix[1][i][j] = 1;
+			Matrix[1][j][i] = 1;
+		} 
+	}
+}
+
+//For debugging.
+void printMatrix(vector<vector<vector<double>>> &Matrix, int current){
+	int gridSize = Matrix[0].size();
+	for (int i = 0; i < gridSize; ++i){
+		for (int j = 0; j < gridSize; ++j){
+			cout << "|";
+			cout << Matrix[current][i][j];
+			cout << "|";
+		}
+		cout << endl;
+	} 
+}
