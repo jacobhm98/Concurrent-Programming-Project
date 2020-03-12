266d917e9295c33c8feeab67942461fe0c08dfeb
jacobhm98 <jacmalm@kth.se>
2020-03-12 22:06:39 +0100

    parallelize the header file

--- Multigrid/Jacobi.h
+++ Multigrid/Jacobi.h
@@ -6,9 +6,12 @@ using std::vector;
 
 class Jacobi {
 	void updateMatrix(vector<vector<vector<double>>> &Matrix);
+	void updateMatrix(int numWorkers, vector<vector<vector<double>>> &Matrix);
 	double maxDiff(vector<vector<vector<double>>> &Matrix);
 	public:
-	void iterate(int numIters, vector<vector<vector<double>>> &Matrix);
-	void iterate(vector<vector<vector<double>>> &Matrix);
-	void printMatrix(vector<vector<vector<double>>> &Matrix, int);
+		void iterate(vector<vector<vector<double>>> &Matrix);
+		void iterate(int numIters, vector<vector<vector<double>>> &Matrix);
+		void iterateP(int numWorkers, vector<vector<vector<double>>> &Matrix);
+		void iterateP(int numWorkers, int numIters, vector<vector<vector<double>>> &Matrix);
+		void printMatrix(vector<vector<vector<double>>> &Matrix, int);
 };
