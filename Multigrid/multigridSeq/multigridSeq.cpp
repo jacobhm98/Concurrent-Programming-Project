37bd64f80605bdce58bd2fb46372afa480e24865
jacobhm98 <jacmalm@kth.se>
2020-03-12 15:26:50 +0100

    interpolate operation done, need to test

--- Multigrid/multigridSeq/multigridSeq.cpp
+++ Multigrid/multigridSeq/multigridSeq.cpp
@@ -71,6 +71,56 @@ int main (int argc, char * argv[]){
 	return 0;
 }
 
+
+void restrict(vector<vector<vector<double>>> &Matrix){
+	int newSize = (Matrix[0].size() + 1) / 2;
+	vector<vector<vector<double>>> tempMatrix;	
+	//initialize the temp matrix to what we want
+	initializeGrid(newSize, tempMatrix);
+	
+	//for each interior point, update it according to G. Andrews textbook p. 550-51
+	for (int i = 1; i < newSize - 1; ++i){
+		for (int j = 1; j < newSize - 1; ++j){
+			int correspondingI = 2*i;
+			int correspondingJ = 2*j;
+			tempMatrix[0][i][j] = Matrix[0][correspondingI][correspondingJ] * 0.5 + (Matrix[0][correspondingI - 1][correspondingJ] + Matrix[0][correspondingI + 1][correspondingJ] + Matrix[0][correspondingI][correspondingJ - 1] + Matrix[0][correspondingI][correspondingJ + 1]) * 0.125;
+
+		}
+	}
+
+	Matrix = tempMatrix;
+}
+
+void interpolate(vector<vector<vector<double>>> &Matrix){
+	int newSize = Matrix[0].size() * 2 - 1;
+	vector<vector<vector<double>>> tempMatrix;
+
+	//initialize the new matrix to size we want + boundary conditions
+	initializeGrid(newSize, tempMatrix);
+
+	//iterate only over directly corresponding points	
+	for (int i = 2; i < newSize - 1; i += 2){
+		for (int j = 2; j < newSize - 1; j += 2){
+			//update the directly corresponding points
+			int correspondingI = i/2;
+			int correspondingJ = j/2;
+			tempMatrix[0][i][j] = Matrix[correspondingI][correspondingJ];
+			
+			//update the points directly next to directly corresponding points
+			tempMatrix[0][i - 1][j] += tempMatrix[0][i][j] * 0.5;
+			tempMatrix[0][i + 1][j] += tempMatrix[0][i][j] * 0.5;
+			tempMatrix[0][i][j - 1] += tempMatrix[0][i][j] * 0.5;
+			tempMatrix[0][i][j + 1] += tempMatrix[0][i][j] * 0.5;
+			
+			//update the adjacent points
+			tempMatrix[0][i - 1][j - 1] += tempMatrix[0][i][j] * 0.25;
+			tempMatrix[0][i - 1][j + 1] += tempMatrix[0][i][j] * 0.25;
+			tempMatrix[0][i + 1][j - 1] += tempMatrix[0][i][j] * 0.25;
+			tempMatrix[0][i + 1][j + 1] += tempMatrix[0][i][j] * 0.25;
+		}
+	}
+}
+
 //a method which takes the gridsize and initializes our matrix to what it's supposed to be (border cells = 1, everything else = 0)
 void initializeGrid(int gridSize, vector<vector<vector<double>>> &Matrix){
 	//initialize the global matrix
@@ -87,25 +137,6 @@ void initializeGrid(int gridSize, vector<vector<vector<double>>> &Matrix){
 	}
 }
 
-void restrict(vector<vector<vector<double>>> &Matrix){
-	int newSize = (Matrix[0].size() + 1) / 2;
-	vector<vector<vector<double>>> tempMatrix;	
-	//initialize the temp matrix to what we want
-	resize(newSize, tempMatrix);
-	setDirichletBoundaryConditions(tempMatrix);
-	for (int i = 1; i < newSize - 1; ++i){
-		for (int j = 1; j < newSize - 1; ++j){
-			int correspondingI = 2*i;
-			int correspondingJ = 2*j;
-			tempMatrix[0][i][j] = Matrix[0][correspondingI][correspondingJ] * 0.5 + (Matrix[0][correspondingI - 1][correspondingJ] + Matrix[0][correspondingI + 1][correspondingJ] + Matrix[0][correspondingI][correspondingJ - 1] + Matrix[0][correspondingI][correspondingJ + 1]) * 0.125;
-
-		}
-	}
-
-	Matrix = tempMatrix;
-	
-
-}
 void resize(int gridSize, vector<vector<vector<double>>> &Matrix){
 	Matrix.resize(2);
 	Matrix[0].resize(gridSize);
