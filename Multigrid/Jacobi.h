539c4633cbdbacc2778a4f5cae85febe8d5d4c9a
jacobhm98 <jacmalm@kth.se>
2020-03-13 14:03:51 +0100

    made maxdiff a public method, and timed the ex. time correctly

--- Multigrid/Jacobi.h
+++ Multigrid/Jacobi.h
@@ -7,11 +7,11 @@ using std::vector;
 class Jacobi {
 	void updateMatrix(vector<vector<vector<double>>> &Matrix);
 	void updateMatrix(int numWorkers, vector<vector<vector<double>>> &Matrix);
-	double maxDiff(vector<vector<vector<double>>> &Matrix);
 	public:
 		void iterate(vector<vector<vector<double>>> &Matrix);
 		void iterate(int numIters, vector<vector<vector<double>>> &Matrix);
 		void iterateP(int numWorkers, vector<vector<vector<double>>> &Matrix);
 		void iterateP(int numWorkers, int numIters, vector<vector<vector<double>>> &Matrix);
+		double maxDiff(vector<vector<vector<double>>> &Matrix);
 		void printMatrix(vector<vector<vector<double>>> &Matrix, int);
 };
--- Multigrid/multigridSeq/multigridSeq.cpp
+++ Multigrid/multigridSeq/multigridSeq.cpp
@@ -45,8 +45,6 @@ int main (int argc, char * argv[]){
 
 	//begin the computations, start the timer right before
 	auto startTime = std::chrono::high_resolution_clock::now();
-	auto endTime = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
 	
 	for (int i = 0; i < 3; ++i){
 		jacobi.iterate(Matrix);
@@ -62,11 +60,14 @@ int main (int argc, char * argv[]){
 		jacobi.iterate(Matrix);
 	}
 	
+	//end the timer of the computations
+	auto endTime = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
 
 	//print out the specified data
 	printf("Grid Size, and Number of Iterations: %d, %d\n", gridSize, numIters);
 	cout << "Execution time of the computational part, in microseconds: " << duration.count() << endl;
-	cout << "The maximum difference of matrix and solution is: " << maxDifference << endl;
+	cout << "The maximum difference of matrix and solution is: " << jacobi.maxDiff(Matrix) << endl;
 	//print out the state of the matrix to filedata.out
 	printMatrixtoFile(Matrix, 0);
 	return 0;
